a150aa2fdc56c2624d94755a9b414a2d14adc73c
endagegnehu <endagegnehu@gmail.com>
2021-09-21 17:11:09 -0500

    change swagger url

--- src/main/java/waa/finalproject/configuration/SpringFoxConfig.java
+++ src/main/java/waa/finalproject/configuration/SpringFoxConfig.java
@@ -13,7 +13,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("edu.miu.ecommerce"))
+                .apis(RequestHandlerSelectors.basePackage("waa.finalproject"))
                 .paths(PathSelectors.any())
                 .build();
     }
